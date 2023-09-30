ee6f53b593bf06352915735ce372489b7b71cc78
nonsomroop <somroopnatthanon@gmail.com>
2023-09-30 18:34:26 +0700

    [Add] todo page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import Home from "./components/Home"
-import Todo from "./components/Todo"
-
 import { Route, Routes } from "react-router-dom"
 import "./App.css"
 import TodoPage from "./pages/Todopage"
 import TodayPage from "./pages/TodayPage"
 import CalendarPage from "./pages/CalendarPage"
-import HomePage from "./pages/HomePage"
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
-          <Route exect path="/" element={<HomePage />} />
+          <Route exect path="/" element={<TodoPage />} />
           <Route path="/todo" element={<TodoPage />} />
           <Route path="/today" element={<TodayPage />} />
           <Route path="/calendar" element={<CalendarPage />} />
-
       </Routes>
     
     </div>
